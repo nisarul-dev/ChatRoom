3a9234457dfc6977d3499bb45dce0867a604e128
nisarul-media <65971192+nisarul-media@users.noreply.github.com>
2021-04-01 23:53:50 +0600

    initial works done but not perfect

---
+++ chatbox/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#action_menu_btn').click(function(){
+        $('.action_menu').toggle();
+    });
+
+    $("#textarea1").emojioneArea({
+        emojiPlaceholder: ":smile_cat:"
+    });
+
+});
